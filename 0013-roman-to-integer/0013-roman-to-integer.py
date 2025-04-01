c67604b4bbd2f2f8944065a92bd3b1588dd5752f
Drew Tullos <116127829+AndrewTullos@users.noreply.github.com>
2025-04-01 13:13:08 -0500

    Time: 6 ms (47.04%), Space: 17.9 MB (21.09%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,15 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+    # largest to smallest: add them 
+    # smaller before larger: subtract smaller
+
+        roman = { "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000 }
+        result = 0
+
+        for i in range(len(s)):
+            if i + 1 < len(s) and roman[s[i]] < roman [s[i + 1]]:
+                result -= roman[s[i]]
+            else:
+                result+= roman[s[i]]
+        
+        return result
