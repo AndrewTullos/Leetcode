16448fb816e2f719046bbea93bb585102cd36ef7
Drew Tullos <116127829+AndrewTullos@users.noreply.github.com>
2025-04-07 15:03:49 -0500

    Time: 6 ms (15.07%), Space: 42.5 MB (31.01%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,58 @@
+import java.math.BigInteger;
+
+class Solution {
+    public String addBinary(String a, String b) {
+        // int intA = binaryToInt(a);
+        // int intB = binaryToInt(b);
+
+        BigInteger bigA = new BigInteger(a, 2);
+        BigInteger bigB = new BigInteger(b, 2);
+
+        BigInteger sum = bigA.add(bigB);
+
+        return sum.toString(2);
+        
+    }
+
+    private static int binaryToInt(String binary) {
+        int result = 0;
+        int power = 0;
+
+        for (int i = binary.length() - 1; i >= 0; i--) {
+            if (binary.charAt(i) == '1') {
+                result += Math.pow(2, power);
+            }
+            power++;
+        }
+        return result;
+    }
+
+    private static String intToBinary(int number) {
+        if (number <= 0) {
+            return "0";
+        }
+
+        String binary = "";
+        int temp = number;
+
+
+        int power = 0;
+        while (temp > 0) {
+            temp /= 2;
+            power++;
+        }
+        power--;
+
+        temp = number;
+        for (int i = power; i >= 0; i--) {
+            if (temp >= Math.pow(2, i)) {
+                binary += "1";
+                temp -= Math.pow(2, i);
+            } else {
+                binary += "0";
+            }
+        }
+
+        return binary;
+    }
+}
