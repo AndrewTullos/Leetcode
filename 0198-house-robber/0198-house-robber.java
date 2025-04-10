9749da651e3867dc7eb2e3a6b8fb6925d31172c5
Drew Tullos <116127829+AndrewTullos@users.noreply.github.com>
2025-04-10 11:23:46 -0500

    Time: 0 ms (100%), Space: 41 MB (60.39%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,6 +1,5 @@
 class Solution {
-    public int rob(int[] nums) {
-
+    public int rob(int[] nums) { 
     //     int even = 0;
     //     int odd = 0;
     //     for (int i = 0; i < nums.length; i++) {
@@ -16,14 +15,14 @@ class Solution {
 
     if (nums.length == 0) return 0;
 
-    int dp[] = new int[nums.length+1];
-    dp[0] = 0;
-    dp[1] = nums[0];
+    int memo[] = new int[nums.length+1];
+    memo[0] = 0;
+    memo[1] = nums[0];
 
     for (int i = 1; i < nums.length; i++) {
-        dp[i+1] = Math.max(dp[i], dp[i-1] + nums[i]);
+        memo[i+1] = Math.max(memo[i], memo[i-1] + nums[i]);
     }
 
-    return dp[nums.length];
+    return memo[nums.length];
     }
 }
