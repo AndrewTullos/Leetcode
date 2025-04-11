f3bc05babb51a784e2349b0229a08dda8cd8c018
Drew Tullos <116127829+AndrewTullos@users.noreply.github.com>
2025-04-11 13:51:26 -0500

    Time: 2 ms (100%), Space: 45.9 MB (84.02%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -0,0 +1,51 @@
+class Solution {
+
+    public int[] topKFrequent(int[] nums, int k) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for (int n : nums) {
+            if (n > max) {
+                max = n;
+            }
+            if (n < min) {
+                min = n;
+            }
+        }
+
+        int[] freq = new int[max - min + 1];
+
+        for (int n : nums) {
+            freq[n - min]++;
+        }
+
+        ArrayList<Integer>[] freqArr = new ArrayList[nums.length+1];
+
+        for (int i=0; i<freq.length; i++) {
+            if (freq[i] > 0) {
+                if (freqArr[freq[i]] == null) {
+                    freqArr[freq[i]] = new ArrayList<Integer>();
+                }
+                freqArr[freq[i]].add(i + min);
+            }
+        }
+        
+        int[] res = new int[k];
+
+        int kk = 0;
+        for (int i=freqArr.length-1; i>=0; i--) {
+            if (freqArr[i] != null) {
+                for (int j = 0; j < freqArr[i].size(); j++) {
+                    res[kk] = freqArr[i].get(j);
+                    kk++;
+
+                    if (kk >= k) {
+                        return res;
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+}
