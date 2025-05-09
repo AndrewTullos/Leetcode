918404429e85c02650714016cdc6c2fae0853a93
Drew Tullos <116127829+AndrewTullos@users.noreply.github.com>
2025-05-09 16:22:10 -0500

    Time: 0 ms (100%), Space: 42.1 MB (14.22%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (!strs[i].startsWith(prefix)) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        }
+        return prefix;
+    }
+}
