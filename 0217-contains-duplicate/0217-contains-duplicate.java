5ff8fbc270dd7d253820cf1393f033a2ab4b9ab2
Drew Tullos <116127829+AndrewTullos@users.noreply.github.com>
2025-05-13 09:28:34 -0500

    Time: 14 ms (49.82%), Space: 58.1 MB (64.72%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,13 +1,16 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> seen = new HashSet<>();
+        HashSet<Integer> numSet = new HashSet<>();
 
-        for (int num : nums) {
-            if (!seen.add(num)) {
+        for (int i = 0; i < nums.length; i++) {
+            if (!numSet.contains(nums[i])) {
+                numSet.add(nums[i]);
+                // System.out.println(nums[i]);
+            } else {
                 return true;
             }
         }
 
         return false;
-        }
+    }
 }
