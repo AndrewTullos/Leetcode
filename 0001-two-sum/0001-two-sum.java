e9314a541c42ef938a14289842c349352765a64e
Drew Tullos <116127829+AndrewTullos@users.noreply.github.com>
2025-05-15 16:06:11 -0500

    Time: 2 ms (98.81%), Space: 45 MB (46.11%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,15 +3,15 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-            // System.out.println(nums[i]);
-            int currentNum = nums[i];
-            int remainder = target - currentNum;
+            int current = nums[i];
+            int remainder = target - current;
 
             if (map.containsKey(remainder)) {
                 return new int[] { map.get(remainder), i };
-            } else {
-                map.put(currentNum, i);
-            }
+            } 
+
+            map.put(nums[i], i);
+        
         }
 
         return null;
