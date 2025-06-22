fa42b3b7f638dbd38242b8ccffeb975366a333d8
Drew Tullos <116127829+AndrewTullos@users.noreply.github.com>
2025-06-22 15:45:05 -0500

    Time: 2 ms (98.93%), Space: 45.3 MB (18.74%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,9 +1,10 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
+        
+         Map<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-            System.out.println(nums[i]);
+            // System.out.println(nums[i]);
 
             int current = nums[i];
             int complement = target - current;
