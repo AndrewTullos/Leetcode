d45a5d4441b8b817e3bb75c61aad95828ffc04bb
Drew Tullos <116127829+AndrewTullos@users.noreply.github.com>
2025-08-26 11:23:28 -0500

    Time: 11 ms (72.42%), Space: 18 MB (34.11%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -4,7 +4,7 @@ class Solution:
             return False
 
         char_count = {}
-        
+
         for char in s:
             char_count[char] = char_count.get(char, 0) + 1
         
@@ -14,5 +14,5 @@ class Solution:
             char_count[char] -= 1
             if char_count[char] == 0:
                 del char_count[char]
-        
+
         return len(char_count) == 0
